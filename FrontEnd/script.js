6bbe521f0f9812728dbc7f66abe134324cd7c5b6
Kalyff <Kalyffsouza@gmail.com>
2025-11-12 14:09:28 -0500

    Melhorar layout

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -4,17 +4,24 @@ async function listar() {
   const res = await fetch(API_URL);
   const dados = await res.json();
 
-  document.getElementById("lista").innerHTML = dados
-    .map(
-      (item) => `
-    <p>
-      <b>${item.name}</b> - ${item.description}
-      <button onclick="editar('${item._id}', '${item.name}', '${item.description}')">Editar</button>
-      <button onclick="remover('${item._id}')">Excluir</button>
-    </p>
-  `
-    )
-    .join("");
+  const lista = document.getElementById("lista");
+  lista.innerHTML = "";
+
+  dados.forEach((item) => {
+    const containerServico = document.createElement("div");
+    containerServico.classList.add("container-servico");
+
+    containerServico.innerHTML = `
+      <div class="container-nome">${item.name}</div>
+      <div class="container-descricao">${item.description}</div>
+      <div class="container-botoes">
+        <button class="btn-editar" onclick="editar('${item._id}', '${item.name}', '${item.description}')">Editar</button>
+        <button class="btn-excluir" onclick="remover('${item._id}')">Excluir</button>
+      </div>
+    `;
+
+    lista.appendChild(containerServico);
+  });
 }
 
 document.getElementById("form").addEventListener("submit", async (e) => {
